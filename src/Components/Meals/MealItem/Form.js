f590ed182da705c8df38ffadeec58ccaf60715a8
ishhaxkaur <kaurxishaa@gmail.com>
2023-10-25 00:59:45 +0530

    Added Cart without functionality

--- src/Components/Meals/MealItem/Form.js
+++ src/Components/Meals/MealItem/Form.js
@@ -1,19 +1,22 @@
-import classes from'./Form.module.css';
-import Input from '../../UI/Input';
-const Form=props=>{
-    return(
-        <form className={classes.form}>
-            <Input label='Amount' input={{
-                id: 'amount',
-                type: 'number',
-                min:'1',
-                max:'5',
-                step: '1',
-                defaultValue: '1'
-            }}/>
-            <button> +Add</button>
-        </form>
-    )
+import classes from "./Form.module.css";
+import Input from "../../UI/Input";
+const Form = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button> +Add</button>
+    </form>
+  );
 };
 
 export default Form;
