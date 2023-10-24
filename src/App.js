c115118cc66236c0c791a375fef3c3e175c3f7b9
ishhaxkaur <kaurxishaa@gmail.com>
2023-10-25 00:40:12 +0530

    Added Summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./Components/Layout/Header";
+import Summary from "./Components/Meals/Summary.js";
 const App = () => {
   return (
     <>
       <Header />
+      <Summary />
     </>
   );
 };
