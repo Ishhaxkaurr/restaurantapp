5b506db071329fba68abeccc794e939242867bf1
ishhaxkaur <kaurxishaa@gmail.com>
2023-10-25 00:46:23 +0530

    dummy meal Items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Components/Layout/Header";
+import DummyMeals from "./Components/Meals/DummyMeals";
 import Summary from "./Components/Meals/Summary.js";
 const App = () => {
   return (
     <>
       <Header />
       <Summary />
+      <DummyMeals />
     </>
   );
 };
